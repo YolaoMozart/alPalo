0c2487af3ff9de038b9c74a3c4b4088f73c8cd2c
Ibn <dislexiarules@gmail.com>
2024-11-05 21:44:01 +0100

    pin pan

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-// Función para cargar el archivo JSON y renderizar los canales
+// Función para cargar el archivo JSON y renderizar los canales por categoría
 async function loadChannels() {
     try {
         const response = await fetch('lista.json');
         const channels = await response.json();
         const channelList = document.getElementById('channel-list');
 
+        // Organiza los canales por categoría
+        const categories = {};
         channels.forEach(channel => {
-            const channelItem = document.createElement('div');
-            channelItem.innerHTML = `<a href="${channel.link}">${channel.name}</a>`;
-            channelList.appendChild(channelItem);
+            if (!categories[channel.category]) {
+                categories[channel.category] = [];
+            }
+            categories[channel.category].push(channel);
         });
+
+        // Itera sobre cada categoría y crea los elementos correspondientes
+        for (const category in categories) {
+            // Crear la cabecera de la categoría
+            const categoryHeader = document.createElement('p');
+            categoryHeader.className = 'lead text-muted mt-5';
+            categoryHeader.textContent = `${category}`;
+            channelList.appendChild(categoryHeader);
+
+            // Crear los elementos de los canales de esta categoría
+            categories[category].forEach(channel => {
+                const channelItem = document.createElement('div');
+                channelItem.innerHTML = `<a href="${channel.link}">${channel.name}</a>`;
+                channelList.appendChild(channelItem);
+            });
+        }
     } catch (error) {
         console.error('Error al cargar los canales:', error);
     }
